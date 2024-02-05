26ad367ff4e62f37822e063ed96f188fa9eee16f
Q.K <139965753+Abjusitsch@users.noreply.github.com>
2024-02-05 13:38:10 +0100

    not working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,18 @@
 // src/app/app.module.ts
 
+// src/app/app.module.ts
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LayoutComponent } from './layout/layout.component';
 
 @NgModule({
-    imports: [BrowserModule, AppRoutingModule],
-    providers: [],
+  
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [],
+  
 })
-export class AppModule {
-    ngDoBootstrap(app) {
-        app.bootstrap(AppComponent);
-    }
-}
+export class AppModule {}
+
